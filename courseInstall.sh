24351ff6fd2922f5c68098470a0263a72f5e96af
markdunning <mark.dunning@gmail.com>
2015-07-09 15:45:54 +0100

    new install script to pull bam file from 1000 genomes

--- courseInstall.sh
+++ courseInstall.sh
@@ -3,18 +3,17 @@ git clone https://github.com/bioinformatics-core-shared-training/cruk-bioinf-ssc
 
 cd cruk-bioinf-sschool
 ##Get example data for sequencing practicals
-wget https://www.dropbox.com/s/ve6o68hkbthe3lo/exampleData.zip
-unzip exampleData.zip
+
+samtools view -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/HG00096/exome_alignment/HG00096.mapped.illumina.mosaik.GBR.exome.20111114.bam 22 | samtools view -bS - > Day2/HG00096.chr22.bam
+samtools index Day2/HG00096.chr22.bam
+ 
+
 
 ##Download required R packages. Assumes R 3.2.0
 R -f installBioCPkgs.R
 ##Remove some files that came from git 
-rm -r images javascripts stylesheets params.json index.html exampleData.zip
+rm -r images img javascripts stylesheets params.json index.html
 
-##Put example data in the right place
-mkdir Day2/exampleData
-mv exampleData/* Day2/exampleData
-rm -r exampleData/
 
 ##Get example breast cancer data for the first practical and put in Practicals folder
 mkdir Day1/nki
@@ -22,3 +21,7 @@ wget https://www.dropbox.com/s/82p2dcwwo3qnf21/nki.zip -P Day1/nki
 cd Day1/nki
 unzip nki.zip
 rm nki.zip
+
+
+# samtools view -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/HG00096/exome_alignment/HG00096.mapped.illumina.mosaik.GBR.exome.20111114.bam 22 | samtools view -bS - > HG00096.chr22.bam
+# samtools index HG00096.chr22.bam
