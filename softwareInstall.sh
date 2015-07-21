7c1ca193d0b53261996b7f4e9d1176098e242bd9
Mark Dunning <markdunning@users.noreply.github.com>
2015-07-21 10:51:20 +0100

    Update softwareInstall.sh

--- softwareInstall.sh
+++ softwareInstall.sh
@@ -1,6 +1,6 @@
 ###Get repository of the course.
 #git clone https://github.com/bioinformatics-core-shared-training/cruk-bioinf-sschool.git
-sudo apt-get install -y git samtools tophat sra-toolkit pkg-config bwa wget bedtools python-dev
+sudo apt-get install -y git samtools tophat sra-toolkit pkg-config bwa wget bedtools python-dev r-base
 
 ##Download required R packages. Assumes R 3.2.0
 R -f installBiocPkgs.R
