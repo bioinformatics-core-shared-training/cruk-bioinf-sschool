48ccf731172de82209ed866abb656d55940921ee
markdunning <mark.dunning@gmail.com>
2015-07-31 09:57:00 +0100

    update bioc install script
    update index to include example data

--- installBiocPkgs.R
+++ installBiocPkgs.R
@@ -3,6 +3,6 @@ options(BioC_mirror = c("Cambridge" = "http://mirrors.ebi.ac.uk/bioconductor/"))
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("Biostrings", "ShortRead", "DESeq", "edgeR","biomaRt", "BSgenome",
            "pasillaBamSubset", "pasilla",
-           "rtracklayer", "ggbio", "vsn","gplots","RColorBrewer","chipseq","htSeqTools","limma","NBPSeq","tweeDEseqCountData","org.Hs.eg.db","Rcade", "ChIPQC","TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19","ChIPpeakAnno","statmod","locfit","Rsubread","breastCancerNKI"))
+           "rtracklayer", "ggbio", "vsn","gplots","RColorBrewer","chipseq","htSeqTools","limma","NBPSeq","tweeDEseqCountData","org.Hs.eg.db","Rcade", "ChIPQC","TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19","ChIPpeakAnno","statmod","locfit","Rsubread","breastCancerNKI","goseq","GO.db"))
 
 ##Also need to install spp_1.11.tar.gz which requires the 'boost' library.
